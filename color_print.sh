710e6c913ee08baae870a385267c233787259145
kilfu0701 <kilfu0701@gmail.com>
2018-12-06 15:19:56 +0900

    add color_print

---
+++ color_print.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# color
+L_Red='\033[1;31m'
+L_Green='\033[1;32m'
+L_Yellow='\033[1;33m'
+L_Blue='\033[1;34m'
+L_Purple='\033[1;35m'
+L_Cyan='\033[1;36m'
+L_White='\033[1;37m'
+NC='\033[0m'
+
+
+
+echo -e "${L_Red} This is Red ${NC}"
+echo -e "${L_Green} This is Green ${NC}"
+echo -e "${L_Yellow} This is Yellow ${NC}"
+echo -e "${L_Blue} This is Blue ${NC}"
+echo -e "${L_Purple} This is Purple ${NC}"
+echo -e "${L_Cyan} This is Cyan ${NC}"
+echo -e "${L_White} This is White ${NC}"
+
+
